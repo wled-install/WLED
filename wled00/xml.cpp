e8632b0896fdaa12fc881eeb2441a6127a62b9cd
Troy <5659019+troyhacks@users.noreply.github.com>
2025-09-01 11:43:10 -0400

    Also don't define PSRAM pins on P4

--- wled00/xml.cpp
+++ wled00/xml.cpp
@@ -232,7 +232,7 @@ void appendGPIOinfo() {
   //Note: Using pin 3 (RX) disables Adalight / Serial JSON
 
   #if defined(ARDUINO_ARCH_ESP32) && defined(BOARD_HAS_PSRAM)
-    #if !defined(CONFIG_IDF_TARGET_ESP32S2) && !defined(CONFIG_IDF_TARGET_ESP32S3) && !defined(CONFIG_IDF_TARGET_ESP32C3)
+    #if !defined(CONFIG_IDF_TARGET_ESP32S2) && !defined(CONFIG_IDF_TARGET_ESP32S3) && !defined(CONFIG_IDF_TARGET_ESP32C3) && !defined(CONFIG_IDF_TARGET_ESP32P4)
     if (psramFound()) oappend(SET_F(",16,17")); // GPIO16 & GPIO17 reserved for SPI RAM on ESP32 (not on S2, S3 or C3)
     #elif defined(CONFIG_IDF_TARGET_ESP32S3)
     if (psramFound()) oappend(SET_F(",33,34,35,36,37")); // in use for "octal" PSRAM or "octal" FLASH -seems that octal PSRAM is very common on S3.
