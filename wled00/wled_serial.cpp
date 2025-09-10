f1def8ae2276ffd326f05aa1d1b87719f776208e
Troy <5659019+troyhacks@users.noreply.github.com>
2025-09-10 15:55:57 -0400

    "R" to Restart got accidentially removed.

--- wled00/wled_serial.cpp
+++ wled00/wled_serial.cpp
@@ -167,6 +167,16 @@ void handleSerial()
           #endif
         } else if (next == 'X') { // WLEDMM - force reconnect via Serial
           forceReconnect = true;
+        } else if (next == 'R') {
+          Serial.print("Rebooting ");
+          for (int i=0;i<5;i++) {
+            Serial.print(".");
+            delay(200);
+          }
+          Serial.println(" now!");
+          Serial.flush();
+          Serial.end();
+          ESP.restart();  // WLEDMM - force reboot via Serial
         } else if (next == 'G') { // WLEDMM - "G"EQ via Serial
           um_data_t *um_data = getAudioData();
           uint8_t fftResult[16] = {0};
