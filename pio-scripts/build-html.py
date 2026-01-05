2d8d478068ae19cd21e381778aeee928441b8e75
Troy <5659019+troyhacks@users.noreply.github.com>
2026-01-05 14:11:30 -0500

    Check all files in HTML data dir for rebuilds

--- pio-scripts/build-html.py
+++ pio-scripts/build-html.py
@@ -8,7 +8,7 @@ if not os.path.isdir('node_modules'):
 if not os.path.exists('wled00/html_ui.h'):
   env.Execute("npm run build")
 
-latest_source = max(glob.glob('wled00/data/*.htm'), key=os.path.getmtime)
+latest_source = max(glob.glob('wled00/data/*'), key=os.path.getmtime)
 latest_export = max(glob.glob('wled00/html_*.h'), key=os.path.getmtime)
 
 if os.path.getmtime(latest_source) > os.path.getmtime(latest_export):
